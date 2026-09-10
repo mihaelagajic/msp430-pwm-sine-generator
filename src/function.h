bdd40a68c4b44a3c842f18f1a8cf0f33ac1bb908
mihaelagajic <gm210249d@student.etf.bg.ac.rs>
2026-09-10 22:56:41 +0200

    Update function.h

--- src/function.h
+++ src/function.h
@@ -1,1 +1,18 @@
+/**
+ * @file function.h
+ * @brief Declaration of function used to write to 7seg display
+ */
 
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+/**
+ * @brief Function used to write to 7seg display
+ * @param digit - value 0-9 to be displayed
+ *
+ * Function writes data a-g on PORT6.
+ * It is assumed that appropriate 7seg display is enabled.
+ */
+extern void WriteLed(unsigned int digit);
+
+#endif /* FUNCTION_H_ */
